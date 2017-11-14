830a92bd0f35a7adf96ad5b81ea43e4f9173a4aa
alex <766f6964+github@gmail.com>
2017-11-14 21:29:59 +0100

    semi functional methods in post class

--- src/Post.php
+++ src/Post.php
@@ -4,43 +4,63 @@
     namespace Sumidero;
 
     class Post {
+        public $id;
+        public $originalPoster;
+        public $permaLink;
+        public $domain;
+        public $externalUrl;
+        public $sub;
+        public $title;
+        public $body;
+        public $thumbnail;
+        public $votes;
+        public $comments;
 
-        public function __construct () {
+        public function __construct (string $id, \Sumidero\User $originalPoster, string $permaLink, string $domain, string $externalUrl, string $sub, string $title, string $body, string $thumbnail) {
+            $this->id = $id;
+            $this->originalPoster = $originalPoster;
+            $this->permaLink = $permaLink;
+            $this->domain = $domain;
+            $this->externalUrl = $externalUrl;
+            $this->sub = $sub;
+            $this->title = $title;
+            $this->body = $body;
+            $this->thumbnail = $thumbnail;
         }
 
         public function __destruct() {
         }
 
-        public static function search($dbh, int $page = 1, int $resultsPage = 16, array $filter = array(), string $order = "") {
-            $results = array();
-            // default anonymous user
-            $users = array(
-                array(
-                    "name" => "anonymous",
-                    "fullname" => "anonymous",
-                    "id" => "0000000",
-                    "avatar" => "https://cdn.shopify.com/s/files/1/2353/2439/products/guy_fawkes_mask.jpg?v=1508552916"
-                ),
+        public function add($dbh) {
+            $params = array(
+                (new \Sumidero\DataBase\DBParam())->str(":id", $this->id),
+                (new \Sumidero\DataBase\DBParam())->str(":original_poster_id", $this->originalPoster->id),
+                (new \Sumidero\DataBase\DBParam())->str(":permalink", $this->permaLink),
+                (new \Sumidero\DataBase\DBParam())->str(":domain", $this->domain),
+                (new \Sumidero\DataBase\DBParam())->str(":external_url", $this->externalUrl),
+                (new \Sumidero\DataBase\DBParam())->str(":sub", $this->sub),
+                (new \Sumidero\DataBase\DBParam())->int(":votes", (rand(1, 2000) * 2 - 2000)),
+                (new \Sumidero\DataBase\DBParam())->int(":comments", rand(0, 16)),
+                (new \Sumidero\DataBase\DBParam())->str(":title", $this->title),
+                (new \Sumidero\DataBase\DBParam())->str(":body", $this->body),
+                (new \Sumidero\DataBase\DBParam())->str(":thumbnail", $this->thumbnail),
             );
-            // get random users from placeholder api
-            $jsonResult = json_decode(file_get_contents("https://randomuser.me/api/?page=1&results=8&inc=name,picture"));
-            foreach($jsonResult->{"results"} as $e) {
-                $users[] = array(
-                    "name" => str_replace(" ", "_", $e->name->first . $e->name->last),
-                    "fullname" => $e->name->first . " " . $e->name->last,
-                    "id" => uniqid(),
-                    "avatar" => $e->picture->large
-                );
-            }
+            $query = '
+                INSERT INTO POST
+                    (id, original_poster_id, creation_date, permalink, domain, external_url, sub, votes, comments, title, body, thumbnail)
+                VALUES
+                    (:id, :original_poster_id, strftime("%s", "now"), :permalink, :domain, :external_url, :sub, :votes, :comments, :title, :body, :thumbnail)
+            ';
+            return($dbh->execute($query, $params));
+        }
 
-            $body = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut leo est, sollicitudin sit amet faucibus a, auctor nec arcu. Donec pulvinar bibendum dictum. Praesent sit amet malesuada urna, in dapibus sem. Integer a neque pulvinar mauris aliquet pretium sit amet at augue. Suspendisse vitae quam egestas, aliquam justo at, vulputate lacus.";
-            // get reddit post data
-
+        public static function search($dbh, int $page = 1, int $resultsPage = 16, array $filter = array(), string $order = "", bool $cache = true) {
+            /*
             $redditCachedJsonFile = dirname(__DIR__) . DIRECTORY_SEPARATOR . "data" . DIRECTORY_SEPARATOR . "reddit.json";
-            if (file_exists($redditCachedJsonFile)) {
+            if (file_exists($redditCachedJsonFile) && $cache) {
                 $redditJson = file_get_contents($redditCachedJsonFile);
             } else {
-                $redditJson = file_get_contents("https://www.reddit.com/.json");
+                $redditJson = file_get_contents(sprintf("https://www.reddit.com%s.json", $subReddits[array_rand($subReddits)]));
                 file_put_contents($redditCachedJsonFile, $redditJson);
             }
             $redditData = json_decode($redditJson);
@@ -60,23 +80,78 @@
                 return($result);
             }
             foreach($redditData->{"data"}->{"children"} as $redditPost) {
+                $id = \Sumidero\UUID::generate(true);
                 $results[] = array(
+                    "id" => $id,
                     "user" => $users[array_rand($users)], // author
                     "title" => $redditPost->{"data"}->{"title"},
                     "domain" => $redditPost->{"data"}->{"domain"},
-                    "body" => $body,
+                    "body" => null,
                     "url" => $redditPost->{"data"}->{"url"},
-                    "permalink" => $redditPost->{"data"}->{"permalink"},
+                    "permalink" => sprintf("/post/%s/%s", $id, \Sumidero\Utils::convertSafeURI($redditPost->{"data"}->{"title"})),
                     "thumbnail" => $redditPost->{"data"}->{"thumbnail"},
                     "created" => $redditPost->{"data"}->{"created"},
-                    "sub" => $redditPost->{"data"}->{"subreddit_name_prefixed"},
+                    "sub" => str_replace("r/", "", $redditPost->{"data"}->{"subreddit_name_prefixed"}),
                     "tags" => getRandomTags(3),
                     "votes" => (rand(1, 2000) * 2 - 2000),
                     "comments" => rand(0, 4)
                 );
                 shuffle($users);
             }
-            return($results);
+            */
+            $results = array();
+            if ($dbh == null) {
+                $dbh = new \Sumidero\Database\DB();
+            }
+            $params = array();
+            $whereCondition = "";
+            if (isset($filter)) {
+            }
+            $queryCount = '
+                SELECT
+                    COUNT (P.id) AS total
+                FROM POST P
+            ';
+            $result = $dbh->query($queryCount, $params);
+            $data = new \stdClass();
+            $data->actualPage = $page;
+            $data->resultsPage = $resultsPage;
+            $data->totalResults = $result[0]->total;
+            $data->totalPages = ceil($data->totalResults / $resultsPage);
+            $sqlOrder = "";
+            if (! empty($order) && $order == "random") {
+                $sqlOrder = " ORDER BY RANDOM() ";
+            } else {
+                $sqlOrder = " ORDER BY P.creation_date DESC ";
+            }
+            $query = sprintf('
+                SELECT
+                    P.id,
+                    P.title,
+                    P.body,
+                    P.thumbnail,
+                    P.creation_date as created,
+                    P.sub,
+                    P.permalink,
+                    P.domain,
+                    P.external_url AS externalUrl,
+                    P.votes,
+                    P.comments,
+                    P.original_poster_id AS userId,
+                    U.name AS userName,
+                    U.full_name AS userFullName,
+                    U.avatar_url AS userAvatar
+                FROM POST P
+                LEFT JOIN USER U ON U.id = P.original_poster_id
+                %s
+                LIMIT %d OFFSET %d
+                ',
+                $sqlOrder,
+                $resultsPage,
+                $resultsPage * ($page - 1)
+            );
+            $data->results = $dbh->query($query, $params);
+            return($data);
         }
     }
 
