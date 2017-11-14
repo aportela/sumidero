fc48b45ed3d62262ff95df2eabb6d2f1712d15cc
alex <766f6964+github@gmail.com>
2017-11-14 23:29:00 +0100

    dynamic subs (from property) & instant search mockup

--- public/scripts/vue-components/navigation-menu.js
+++ public/scripts/vue-components/navigation-menu.js
@@ -18,10 +18,7 @@ var vTemplateNavigationMenu = function () {
                         <span>subs</span>
                     </a>
                     <div class="navbar-dropdown">
-                        <a class="navbar-item" href="/#/s/">/s/ (frontpage)</a>
-                        <a class="navbar-item" href="/#/s/news">/s/news</a>
-                        <a class="navbar-item" href="/#/s/television">/s/television</a>
-                        <a class="navbar-item" href="/#/s/games">/s/games</a>
+                        <a v-for="sub in subs" class="navbar-item" v-bind:href="sub.path">{{ sub.name }}</a>
                     </div>
                 </div>
                 <a class="navbar-item" href="/#/add-post">
@@ -31,8 +28,8 @@ var vTemplateNavigationMenu = function () {
                     <span>add post</span>
                 </a>
                 <div class="navbar-item">
-                    <div class="control has-icons-left">
-                        <input class="input" type="text" placeholder="search">
+                    <div class="control has-icons-left" v-bind:class="searching ? 'is-loading': ''">
+                        <input class="input" type="text" :disabled="searching" placeholder="search" v-model="searchText" v-on:keyup.esc="abortInstantSearch();" v-on:keyup="instantSearch();">
                         <span class="icon is-small is-left">
                             <i class="fa fa-search"></i>
                         </span>
@@ -96,8 +93,54 @@ var navigationMenu = Vue.component('sumidero-navigation-menu-component', {
     template: vTemplateNavigationMenu(),
     data: function () {
         return ({
-            userName: "aportela"
+            searchText: null,
+            searchTimeout: null,
+            searching: false,
+            userName: "aportela",
+            subs: [
+                {
+                    name: "/s/ (frontpage)",
+                    path: "/#/s/"
+                },
+                {
+                    name: "/s/news",
+                    path: "/#/s/news"
+                },
+                {
+                    name: "/s/television",
+                    path: "/#/s/television"
+                },
+                {
+                    name: "/s/games",
+                    path: "/#/s/games"
+                }
+            ]
         });
     },
-    props: [ 'logged' ]
+    props: [ 'logged' ],
+    methods: {
+        abortInstantSearch: function() {
+            this.searchText = null;
+        },
+        instantSearch: function() {
+            if (this.searchText) {
+                var self = this;
+                if (self.searchTimeout) {
+                    clearTimeout(self.searchTimeout);
+                }
+                self.searchTimeout = setTimeout(function () {
+                    self.search();
+                }, 128);
+            } else {
+                this.abortInstantSearch();
+            }
+        },
+        search: function() {
+            var self = this;
+            self.searching = true;
+            setTimeout(function () {
+                self.searching = false;
+            }, 500);
+        }
+    }
 });
