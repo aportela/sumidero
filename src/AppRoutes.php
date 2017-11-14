f27372d7d5b10a2be7fad5655b7f961e4432234c
alex <766f6964+github@gmail.com>
2017-11-14 21:28:49 +0100

    changes in routes for posts entities

--- src/AppRoutes.php
+++ src/AppRoutes.php
@@ -16,17 +16,42 @@
         return $response->withJson(['success' => true], 200);
     })->add(new \Sumidero\Middleware\APIExceptionCatcher);
 
-
     $app->get('/api/posts', function (Request $request, Response $response, array $args) {
         $this->logger->info("Slim-Skeleton GET '/api/posts' route");
-        $results = \Sumidero\Post::search(null, 1, 16, array(), "");
-        return $response->withJson(['posts' => $results ], 200);
+        $data = \Sumidero\Post::search(null, 1, 16, array(), "random");
+        return $response->withJson(['posts' => $data->results ], 200);
     })->add(new \Sumidero\Middleware\APIExceptionCatcher);
 
     $app->post('/api/post/scrap', function (Request $request, Response $response, array $args) {
         $this->logger->info("Slim-Skeleton GET '/api/post/scrap' route");
         $data = (new \Sumidero\Scraper())->scrap($request->getParam("url", ""));
         return $response->withJson(['title' => $data["title"], 'image' => $data["image"], 'body' => $data["article"]->textContent ? trim($data["article"]->textContent): null ], 200);
+    })->add(new \Sumidero\Middleware\APIExceptionCatcher);
+
+    $app->post('/api/post/add', function (Request $request, Response $response, array $args) {
+        $this->logger->info("Slim-Skeleton POST '/api/post/add' route");
+        $dbh = new \Sumidero\Database\DB();
+        $user = \Sumidero\User::getRandom();
+        $user->add($dbh);
+        $externalUrl = $request->getParam("externalUrl", "");
+        if (! empty($externalUrl)) {
+            $subReddits = array("all", "todayilearned", "gaming", "movies", "mildlyinteresting", "science", "OldSchoolCool");
+            shuffle($subReddits);
+            $data = (new \Sumidero\Scraper())->scrap($externalUrl);
+            $post = new \Sumidero\Post(
+                \Sumidero\UUID::generate(true),
+                $user,
+                $externalUrl . uniqid(),
+                parse_url($externalUrl, PHP_URL_HOST),
+                $externalUrl,
+                $subReddits[array_rand($subReddits)],
+                $data["title"],
+                $data["article"]->textContent ? substr(trim($data["article"]->textContent), 0, 384): "",
+                $data["image"] ? $data["image"]: ""
+            );
+            $post->add($dbh);
+        }
+        return $response->withJson(['id' => $post->id, 'title' => $post->title, 'image' => $post->thumbnail, 'body' => $post->body ], 200);
     })->add(new \Sumidero\Middleware\APIExceptionCatcher);
 
 ?>
