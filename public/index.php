c8099f4ec802e680a173c27c1825601a24873b24
alex <766f6964+github@gmail.com>
2017-11-19 19:19:37 +0100

    app scope changed

--- public/index.php
+++ public/index.php
@@ -18,12 +18,7 @@
 
     session_start();
 
-    $settings = require __DIR__ . '/../src/AppSettings.php';
-
-    $app = (new \Sumidero\App($settings))->get();
-
-    require __DIR__ . '/../src/AppDependencies.php';
-    require __DIR__ . '/../src/AppRoutes.php';
+    $app = (new \Sumidero\App())->get();
 
     $app->run();
 
