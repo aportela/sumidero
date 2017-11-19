3feb4662a7600326611f7f181484c02eaeb20ec5
alex <766f6964+github@gmail.com>
2017-11-19 19:23:13 +0100

    added default tests

---
+++ src/Test/UserTest.php
@@ -0,0 +1,33 @@
+<?php
+
+    declare(strict_types=1);
+
+    namespace Sumidero\Test;
+
+    require_once dirname(dirname(__DIR__)) . DIRECTORY_SEPARATOR . "vendor" . DIRECTORY_SEPARATOR . "autoload.php";
+
+    final class UserTest extends \PHPUnit\Framework\TestCase
+    {
+        /**
+         * Called once just like normal constructor
+         */
+        public static function setUpBeforeClass () { }
+
+        /**
+         * Initialize the test case
+         * Called for every defined test
+         */
+        public  function setUp() { }
+
+        /**
+         * Clean up the test case, called for every defined test
+         */
+        public function tearDown() { }
+
+        /**
+         * Clean up the whole test class
+         */
+        public static function tearDownAfterClass() { }
+
+    }
+?>
