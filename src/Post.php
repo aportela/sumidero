9a593237aaf9c1ff2c9bfeed043b9f6af0fdbf52
alex <766f6964+github@gmail.com>
2017-12-05 21:09:17 +0100

    changes on post class & added post testing class

--- src/Post.php
+++ src/Post.php
@@ -5,113 +5,102 @@
 
     class Post {
         public $id;
-        public $originalPoster;
+        public $opUserId;
         public $permaLink;
-        public $domain;
         public $externalUrl;
+        public $domain;
         public $sub;
         public $title;
         public $body;
         public $thumbnail;
-        public $votes;
-        public $comments;
+        public $totalVotes;
+        public $totalComments;
 
-        public function __construct (string $id, \Sumidero\User $originalPoster, string $permaLink, string $domain, string $externalUrl, string $sub, string $title, string $body, string $thumbnail) {
-            $this->id = $id;
-            $this->originalPoster = $originalPoster;
-            $this->permaLink = $permaLink;
-            $this->domain = $domain;
-            $this->externalUrl = $externalUrl;
-            $this->sub = $sub;
-            $this->title = $title;
-            $this->body = $body;
-            $this->thumbnail = $thumbnail;
-        }
+        public function __construct () { }
 
-        public function __destruct() {
-        }
+        public function __destruct() { }
 
-        public function add($dbh) {
+        public function add(\Sumidero\Database\DB $dbh) {
+            if (empty($this->id)) {
+                throw new \Sumidero\Exception\InvalidParamsException("id");
+            }
+            if (empty($this->permaLink)) {
+                throw new \Sumidero\Exception\InvalidParamsException("permaLink");
+            }
+            if (empty($this->title)) {
+                throw new \Sumidero\Exception\InvalidParamsException("title");
+            }
             $params = array(
                 (new \Sumidero\Database\DBParam())->str(":id", $this->id),
-                (new \Sumidero\Database\DBParam())->str(":original_poster_id", $this->originalPoster->id),
+                (new \Sumidero\Database\DBParam())->str(":original_poster_id", \Sumidero\UserSession::getUserId()),
                 (new \Sumidero\Database\DBParam())->str(":permalink", $this->permaLink),
-                (new \Sumidero\Database\DBParam())->str(":domain", $this->domain),
-                (new \Sumidero\Database\DBParam())->str(":external_url", $this->externalUrl),
-                (new \Sumidero\Database\DBParam())->str(":sub", $this->sub),
-                (new \Sumidero\Database\DBParam())->int(":votes", (rand(1, 2000) * 2 - 2000)),
-                (new \Sumidero\Database\DBParam())->int(":comments", rand(0, 16)),
                 (new \Sumidero\Database\DBParam())->str(":title", $this->title),
-                (new \Sumidero\Database\DBParam())->str(":body", $this->body),
-                (new \Sumidero\Database\DBParam())->str(":thumbnail", $this->thumbnail),
             );
+            if (! empty($this->externalUrl)) {
+                if (filter_var($this->externalUrl, FILTER_VALIDATE_URL)) {
+                    $params[] = (new \Sumidero\Database\DBParam())->str(":external_url", $this->externalUrl);
+                    $params[] = (new \Sumidero\Database\DBParam())->str(":domain", parse_url($this->externalUrl, PHP_URL_HOST));
+                } else {
+                    throw new \Sumidero\Exception\InvalidParamsException("externalUrl");
+                }
+            } else {
+                $params[] = (new \Sumidero\Database\DBParam())->null(":external_url");
+                $params[] = (new \Sumidero\Database\DBParam())->null(":domain");
+            }
+            if (! empty($this->sub)) {
+                $params[] = (new \Sumidero\Database\DBParam())->str(":sub", $this->sub);
+            } else {
+                $params[] = (new \Sumidero\Database\DBParam())->null(":sub");
+            }
+            if (! empty($this->body)) {
+                $params[] = (new \Sumidero\Database\DBParam())->str(":body", $this->body);
+            } else {
+                $params[] = (new \Sumidero\Database\DBParam())->null(":body");
+            }
+            if (! empty($this->thumbnail)) {
+                $params[] = (new \Sumidero\Database\DBParam())->str(":thumbnail", $this->thumbnail);
+            } else {
+                $params[] = (new \Sumidero\Database\DBParam())->null(":thumbnail");
+            }
             $query = '
                 INSERT INTO POST
-                    (id, original_poster_id, creation_date, permalink, domain, external_url, sub, votes, comments, title, body, thumbnail)
+                    (id, original_poster_id, creation_date, permalink, domain, external_url, sub, total_votes, total_comments, title, body, thumbnail)
                 VALUES
-                    (:id, :original_poster_id, strftime("%s", "now"), :permalink, :domain, :external_url, :sub, :votes, :comments, :title, :body, :thumbnail)
+                    (:id, :original_poster_id, strftime("%s", "now"), :permalink, :domain, :external_url, :sub, 0, 0, :title, :body, :thumbnail)
             ';
             return($dbh->execute($query, $params));
         }
 
-        public static function search($dbh, int $page = 1, int $resultsPage = 16, array $filter = array(), string $order = "", bool $cache = true) {
-            /*
-            $redditCachedJsonFile = dirname(__DIR__) . DIRECTORY_SEPARATOR . "data" . DIRECTORY_SEPARATOR . "reddit.json";
-            if (file_exists($redditCachedJsonFile) && $cache) {
-                $redditJson = file_get_contents($redditCachedJsonFile);
-            } else {
-                $redditJson = file_get_contents(sprintf("https://www.reddit.com%s.json", $subReddits[array_rand($subReddits)]));
-                file_put_contents($redditCachedJsonFile, $redditJson);
-            }
-            $redditData = json_decode($redditJson);
-            //print_r($redditData->{"data"}->{"children"}[0]->{"data"}); exit;
-
-            function getRandomTags(int $count) {
-                $tags = array(
-                    "videos", "news", "article", "php", "funny", "pics", "gaming", "hardware", "music", "javascript"
-                );
-                $result = array();
-                for($i = 0; $i < $count; $i++) {
-                    $t = $tags[array_rand($tags)];
-                    $result[] = $t;
-                    shuffle($tags);
-                }
-                $result = array_unique($result);
-                return($result);
-            }
-            foreach($redditData->{"data"}->{"children"} as $redditPost) {
-                $id = (\Ramsey\Uuid\Uuid::uuid4())->toString();
-                $results[] = array(
-                    "id" => $id,
-                    "user" => $users[array_rand($users)], // author
-                    "title" => $redditPost->{"data"}->{"title"},
-                    "domain" => $redditPost->{"data"}->{"domain"},
-                    "body" => null,
-                    "url" => $redditPost->{"data"}->{"url"},
-                    "permalink" => sprintf("/post/%s/%s", $id, \Sumidero\Utils::convertSafeURI($redditPost->{"data"}->{"title"})),
-                    "thumbnail" => $redditPost->{"data"}->{"thumbnail"},
-                    "created" => $redditPost->{"data"}->{"created"},
-                    "sub" => str_replace("r/", "", $redditPost->{"data"}->{"subreddit_name_prefixed"}),
-                    "tags" => getRandomTags(3),
-                    "votes" => (rand(1, 2000) * 2 - 2000),
-                    "comments" => rand(0, 4)
-                );
-                shuffle($users);
-            }
-            */
+        public static function search(\Sumidero\Database\DB $dbh, int $page = 1, int $resultsPage = 16, array $filter = array(), string $order = "") {
             $results = array();
-            if ($dbh == null) {
-                $dbh = new \Sumidero\Database\DB();
-            }
             $params = array();
             $whereCondition = "";
+            $queryConditions = array();
             if (isset($filter)) {
+                if (isset($filter["sub"]) && ! empty($filter["sub"])) {
+                    $queryConditions[] = " P.sub LIKE :sub ";
+                    $params[] = (new \Sumidero\Database\DBParam())->str(":sub", $filter["sub"]);
+                }
+                if (isset($filter["domain"]) && ! empty($filter["domain"])) {
+                    $queryConditions[] = " P.domain LIKE :domain ";
+                    $params[] = (new \Sumidero\Database\DBParam())->str(":domain", $filter["domain"]);
+                }
+                if (isset($filter["title"]) && ! empty($filter["title"])) {
+                    $queryConditions[] = " P.title LIKE :title ";
+                    $params[] = (new \Sumidero\Database\DBParam())->str(":title", "%" . $filter["title"] . "%");
+                }
+                if (isset($filter["body"]) && ! empty($filter["body"])) {
+                    $queryConditions[] = " P.body LIKE :body ";
+                    $params[] = (new \Sumidero\Database\DBParam())->str(":body", "%" . $filter["body"] . "%");
+                }
+                $whereCondition = count($queryConditions) > 0 ? " WHERE " .  implode(" AND ", $queryConditions) : "";
             }
-            $queryCount = '
+            $queryCount = sprintf('
                 SELECT
                     COUNT (P.id) AS total
                 FROM POST P
-            ';
+                %s
+            ', $whereCondition);
             $result = $dbh->query($queryCount, $params);
             $data = new \stdClass();
             $data->actualPage = $page;
@@ -135,17 +124,18 @@
                     P.permalink,
                     P.domain,
                     P.external_url AS externalUrl,
-                    P.votes,
-                    P.comments,
+                    P.total_votes AS totalVotes,
+                    P.total_comments AS totalComments,
                     P.original_poster_id AS userId,
-                    U.name AS userName,
-                    U.full_name AS userFullName,
-                    U.avatar_url AS userAvatar
+                    U.nick AS userNick,
+                    U.avatar_url AS userAvatarUrl
                 FROM POST P
                 LEFT JOIN USER U ON U.id = P.original_poster_id
                 %s
+                %s
                 LIMIT %d OFFSET %d
                 ',
+                $whereCondition,
                 $sqlOrder,
                 $resultsPage,
                 $resultsPage * ($page - 1)
---
+++ src/Test/PostTest.php
@@ -0,0 +1,133 @@
+<?php
+
+    declare(strict_types=1);
+
+    namespace Sumidero\Test;
+
+    require_once dirname(dirname(__DIR__)) . DIRECTORY_SEPARATOR . "vendor" . DIRECTORY_SEPARATOR . "autoload.php";
+
+    final class PostTest extends \PHPUnit\Framework\TestCase
+    {
+        static private $app = null;
+        static private $container = null;
+        static private $dbh = null;
+
+        /**
+         * Called once just like normal constructor
+         */
+        public static function setUpBeforeClass () {
+            self::$app = (new \Sumidero\App())->get();
+            self::$container = self::$app->getContainer();
+            self::$dbh = new \Sumidero\Database\DB(self::$container);
+        }
+
+        /**
+         * Initialize the test case
+         * Called for every defined test
+         */
+        public function setUp() {
+            self::$dbh->beginTransaction();
+        }
+
+        /**
+         * Clean up the test case, called for every defined test
+         */
+        public function tearDown() {
+            self::$dbh->rollBack();
+        }
+
+        /**
+         * Clean up the whole test class
+         */
+        public static function tearDownAfterClass() {
+            self::$dbh = null;
+            self::$container = null;
+            self::$app = null;
+        }
+
+        private function createUserAndLogin(): string {
+            $id = (\Ramsey\Uuid\Uuid::uuid4())->toString();
+            $u = new \Sumidero\User($id, $id . "@server.com", "secret", $id, "");
+            $u->add(self::$dbh);
+            $u->login(self::$dbh);
+            return($id);
+        }
+
+        public function testAddWithoutId(): void {
+            $this->expectException(\Sumidero\Exception\InvalidParamsException::class);
+            $this->expectExceptionMessage("id");
+            $this->createUserAndLogin();
+            $post = new \Sumidero\Post();
+            $post->add(self::$dbh);
+        }
+
+        public function testAddWithoutPermalink(): void {
+            $this->expectException(\Sumidero\Exception\InvalidParamsException::class);
+            $this->expectExceptionMessage("permaLink");
+            $opUserId = $this->createUserAndLogin();
+            $post = new \Sumidero\Post();
+            $post->id = (\Ramsey\Uuid\Uuid::uuid4())->toString();
+            $post->opUserId = $opUserId;
+            $post->add(self::$dbh);
+        }
+
+        public function testAddWithoutTitle(): void {
+            $this->expectException(\Sumidero\Exception\InvalidParamsException::class);
+            $this->expectExceptionMessage("title");
+            $opUserId = $this->createUserAndLogin();
+            $post = new \Sumidero\Post();
+            $post->id = (\Ramsey\Uuid\Uuid::uuid4())->toString();
+            $post->opUserId = $opUserId;
+            $post->permaLink = "/s/test" . $opUserId;
+            $post->add(self::$dbh);
+        }
+
+        public function testAddWithInvalidExternalUrl(): void {
+            $this->expectException(\Sumidero\Exception\InvalidParamsException::class);
+            $this->expectExceptionMessage("externalUrl");
+            $opUserId = $this->createUserAndLogin();
+            $post = new \Sumidero\Post();
+            $post->id = (\Ramsey\Uuid\Uuid::uuid4())->toString();
+            $post->opUserId = $opUserId;
+            $post->permaLink = "/s/test" . $opUserId;
+            $post->title = "post title from " . $opUserId;
+            $post->externalUrl = "htp_s//not.a.valid-url";
+            $post->add(self::$dbh);
+        }
+
+        public function testAdd(): void {
+            $opUserId = $this->createUserAndLogin();
+            $post = new \Sumidero\Post();
+            $post->id = (\Ramsey\Uuid\Uuid::uuid4())->toString();
+            $post->opUserId = $opUserId;
+            $post->permaLink = "/s/test" . $opUserId;
+            $post->title = "post title from " . $opUserId;
+            $this->assertTrue($post->add(self::$dbh));
+        }
+
+        public function testSearchWithSubFilter(): void {
+            $this->assertInstanceOf("stdclass", \Sumidero\Post::search(self::$dbh, 1, 16, array("sub" => "test"), ""));
+        }
+
+        public function testSearchWithDomainFilter(): void {
+            $this->assertInstanceOf("stdclass", \Sumidero\Post::search(self::$dbh, 1, 16, array("domain" => "www.server.com"), ""));
+        }
+
+        public function testSearchWithTitleFilter(): void {
+            $this->assertInstanceOf("stdclass", \Sumidero\Post::search(self::$dbh, 1, 16, array("title" => "test"), ""));
+        }
+
+        public function testSearchWithBodyFilter(): void {
+            $this->assertInstanceOf("stdclass", \Sumidero\Post::search(self::$dbh, 1, 16, array("body" => "test"), ""));
+        }
+
+        public function testSearchWithoutFilters(): void {
+            $this->assertInstanceOf("stdclass", \Sumidero\Post::search(self::$dbh, 1, 16, array(), ""));
+        }
+
+        public function testSearchWithAllFilters(): void {
+            $this->assertInstanceOf("stdclass", \Sumidero\Post::search(self::$dbh, 1, 16, array("sub" => "test", "domain" => "www.server.com", "title" => "test", "body" => "test"), ""));
+        }
+
+    }
+?>
