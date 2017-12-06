c2fea26182a4e1db621b943524bab0e2a775b27d
alex <766f6964+github@gmail.com>
2017-12-06 20:34:31 +0100

    re-organizing routes

--- src/AppRoutes.php
+++ src/AppRoutes.php
@@ -4,8 +4,29 @@
     use Slim\Http\Request;
     use Slim\Http\Response;
 
-    $this->app->group("/api", function() {
+    $this->app->get('/', function (Request $request, Response $response, array $args) {
+        $this->logger->info($request->getOriginalMethod() . " " . $request->getUri()->getPath());
+        $v = new \Sumidero\Database\Version(new \Sumidero\Database\DB($this), $this->get('settings')['database']['type']);
+        return $this->view->render($response, 'index.html.twig', array(
+            'settings' => $this->settings["twigParams"],
+            'initialState' => json_encode(
+                array(
+                    "logged" => \Sumidero\UserSession::isLogged(),
+                    "session" => array(
+                        "userId" => \Sumidero\UserSession::getUserId(),
+                        "email" => \Sumidero\UserSession::getEmail(),
+                        "nick" => \Sumidero\UserSession::getNick(),
+                        "avatarUrl" => \Sumidero\UserSession::getAvatarUrl(),
+                    ),
+                    'upgradeAvailable' => $v->hasUpgradeAvailable(),
+                    "defaultResultsPage" => $this->get('settings')['common']['defaultResultsPage'],
+                    "allowSignUp" => $this->get('settings')['common']['allowSignUp']
+                )
+            )
+        ));
+    });
 
+    $this->app->group("/api", function() {
         /* user */
 
         $this->group("/user", function() {
@@ -15,32 +36,47 @@
             });
 
             $this->post('/signin', function (Request $request, Response $response, array $args) {
-                $u = new \Sumidero\User("", $request->getParam("email", ""), $request->getParam("password", ""));
-                if ($u->login(new \Sumidero\Database\DB($this))) {
-                    return $response->withJson(['logged' => true], 200);
+                $u = new \Sumidero\User("", $request->getParam("email", ""), $request->getParam("password", ""), $request->getParam("nick", ""), "");
+                $dbh = new \Sumidero\Database\DB($this);
+                if ($u->login($dbh)) {
+                    $v = new \Sumidero\Database\Version($dbh, $this->get('settings')['database']['type']);
+                    return $response->withJson(
+                        array(
+                            'initialState' => array(
+                                "logged" => \Sumidero\UserSession::isLogged(),
+                                "session" => array(
+                                    "userId" => \Sumidero\UserSession::getUserId(),
+                                    "email" => \Sumidero\UserSession::getEmail(),
+                                    "nick" => \Sumidero\UserSession::getNick(),
+                                    "avatarUrl" => \Sumidero\UserSession::getAvatarUrl(),
+                                ),
+                                'upgradeAvailable' => $v->hasUpgradeAvailable(),
+                                "defaultResultsPage" => $this->get('settings')['common']['defaultResultsPage'],
+                                "allowSignUp" => $this->get('settings')['common']['allowSignUp']
+                            )
+                        )
+                    , 200);
                 } else {
-                    return $response->withJson(['logged' => false], 401);
+                    return $response->withJson([], 401);
                 }
             });
 
             $this->post('/signup', function (Request $request, Response $response, array $args) {
                 if ($this->get('settings')['common']['allowSignUp']) {
                     $dbh = new \Sumidero\Database\DB($this);
-                    $u = new \Sumidero\User(
-                        "",
-                        $request->getParam("email", ""),
-                        $request->getParam("password", "")
-                    );
                     $exists = false;
-                    try {
-                        $u->get($dbh);
-                        $exists = true;
-                    } catch (\Sumidero\Exception\NotFoundException $e) {
-                    }
-                    if ($exists) {
-                        return $response->withJson([], 409);
+                    if (\Sumidero\User::findByEmail($dbh, $request->getParam("email", "")) != null) {
+                        return $response->withJson(array("invalidOrMissingParams" => array("email")), 409);
+                    } else if (\Sumidero\User::findByNick($dbh, $request->getParam("nick", "")) != null) {
+                        return $response->withJson(array("invalidOrMissingParams" => array("nick")), 409);
                     } else {
-                        $u->id = (\Ramsey\Uuid\Uuid::uuid4())->toString();
+                        $u = new \Sumidero\User(
+                            (\Ramsey\Uuid\Uuid::uuid4())->toString(),
+                            $request->getParam("email", ""),
+                            $request->getParam("password", ""),
+                            $request->getParam("nick", ""),
+                            ""
+                        );
                         $u->add($dbh);
                         return $response->withJson([], 200);
                     }
@@ -51,33 +87,36 @@
 
             $this->get('/signout', function (Request $request, Response $response, array $args) {
                 \Sumidero\User::logout();
-                return $response->withJson(['logged' => false], 200);
+                $v = new \Sumidero\Database\Version(new \Sumidero\Database\DB($this), $this->get('settings')['database']['type']);
+                return $response->withJson(
+                    array(
+                        'initialState' => array(
+                            "logged" => \Sumidero\UserSession::isLogged(),
+                            "session" => array(
+                                "userId" => \Sumidero\UserSession::getUserId(),
+                                "email" => \Sumidero\UserSession::getEmail(),
+                                "nick" => \Sumidero\UserSession::getNick(),
+                                "avatarUrl" => \Sumidero\UserSession::getAvatarUrl(),
+                            ),
+                            'upgradeAvailable' => $v->hasUpgradeAvailable(),
+                            "defaultResultsPage" => $this->get('settings')['common']['defaultResultsPage'],
+                            "allowSignUp" => $this->get('settings')['common']['allowSignUp']
+                        )
+                    )
+                , 200);
             });
         });
 
         /* user */
+
+        $this->group("/post", function() {
+
+        })->add(new \Sumidero\Middleware\CheckAuth($this->getContainer()));
+
     })->add(new \Sumidero\Middleware\APIExceptionCatcher($this->app->getContainer()));
 
-    $this->app->get('/', function (Request $request, Response $response, array $args) {
-        $this->logger->info($request->getOriginalMethod() . " " . $request->getUri()->getPath());
-        $v = new \Sumidero\Database\Version(new \Sumidero\Database\DB($this), $this->get('settings')['database']['type']);
-        return $this->view->render($response, 'index.html.twig', array(
-            'settings' => $this->settings["twigParams"],
-            'initialState' => json_encode(
-                array(
-                    "logged" => \Sumidero\User::isLogged(),
-                    'upgradeAvailable' => $v->hasUpgradeAvailable(),
-                    "defaultResultsPage" => $this->get('settings')['common']['defaultResultsPage'],
-                    "allowSignUp" => $this->get('settings')['common']['allowSignUp']
-                )
-            )
-        ));
-    });
-
+    /*
     $this->app->group("/api", function() {
-        $this->get('/poll', function (Request $request, Response $response, array $args) {
-            return $response->withJson(['success' => true], 200);
-        });
 
         $this->get('/posts', function (Request $request, Response $response, array $args) {
             $data = \Sumidero\Post::search(new \Sumidero\Database\DB($this), 1, 16, array(), "random");
@@ -114,5 +153,5 @@
             return $response->withJson(['id' => $post->id, 'title' => $post->title, 'image' => $post->thumbnail, 'body' => $post->body ], 200);
         });
     })->add(new \Sumidero\Middleware\APIExceptionCatcher($this->app->getContainer()));
-
+    */
 ?>
