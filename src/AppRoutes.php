bf45527defc7a332c989d8406b6f8395d2a7f1b9
alex <766f6964+github@gmail.com>
2017-12-09 17:41:53 +0100

    updated routes to match new code

--- src/AppRoutes.php
+++ src/AppRoutes.php
@@ -6,7 +6,8 @@
 
     $this->app->get('/', function (Request $request, Response $response, array $args) {
         $this->logger->info($request->getOriginalMethod() . " " . $request->getUri()->getPath());
-        $v = new \Sumidero\Database\Version(new \Sumidero\Database\DB($this), $this->get('settings')['database']['type']);
+        $dbh = new \Sumidero\Database\DB($this);
+        $v = new \Sumidero\Database\Version($dbh, $this->get('settings')['database']['type']);
         return $this->view->render($response, 'index.html.twig', array(
             'settings' => $this->settings["twigParams"],
             'initialState' => json_encode(
@@ -18,6 +19,7 @@
                         "nick" => \Sumidero\UserSession::getNick(),
                         "avatarUrl" => \Sumidero\UserSession::getAvatarUrl(),
                     ),
+                    "subs" => \Sumidero\Post::searchSubs($dbh),
                     'upgradeAvailable' => $v->hasUpgradeAvailable(),
                     "defaultResultsPage" => $this->get('settings')['common']['defaultResultsPage'],
                     "allowSignUp" => $this->get('settings')['common']['allowSignUp']
@@ -50,6 +52,7 @@
                                     "nick" => \Sumidero\UserSession::getNick(),
                                     "avatarUrl" => \Sumidero\UserSession::getAvatarUrl(),
                                 ),
+                                "subs" => \Sumidero\Post::searchSubs($dbh),
                                 'upgradeAvailable' => $v->hasUpgradeAvailable(),
                                 "defaultResultsPage" => $this->get('settings')['common']['defaultResultsPage'],
                                 "allowSignUp" => $this->get('settings')['common']['allowSignUp']
@@ -109,49 +112,65 @@
 
         /* user */
 
+        /* post */
+
         $this->group("/post", function() {
 
+            $this->post('/scrap', function (Request $request, Response $response, array $args) {
+                $data = (new \Sumidero\Scraper())->scrap($request->getParam("url", ""));
+                return $response->withJson([
+                    'title' => isset($data["title"]) ? $data["title"]: null,
+                    'image' => isset($data["image"]) ? $data["image"]: null,
+                    'body' => isset($data["article"]) && $data["article"]->textContent ? trim($data["article"]->textContent): null
+                ], 200);
+            });
+
+            $this->post('/add', function (Request $request, Response $response, array $args) {
+                $post = new \Sumidero\Post();
+                $post->id = (\Ramsey\Uuid\Uuid::uuid4())->toString();
+                $post->opUserId = \Sumidero\UserSession::getUserId();
+                $externalUrl = $request->getParam("externalUrl", "");
+                $post->sub = $request->getParam("sub", "");
+                $post->tags = $request->getParam("tags", array());
+                $post->permaLink = uniqid();
+                if (! empty($externalUrl) && filter_var($externalUrl, FILTER_VALIDATE_URL)) {
+                    $data = (new \Sumidero\Scraper())->scrap($externalUrl);
+                    $post->permaLink = $externalUrl . uniqid();
+                    $post->externalUrl = $externalUrl;
+                    $post->domain = parse_url($externalUrl, PHP_URL_HOST);
+
+                    $post->title  = $data["title"];
+                    $post->body = $data["article"]->textContent ? substr(trim($data["article"]->textContent), 0, 384): "";
+                    $post->thumbnail = $data["image"] ? $data["image"]: "";
+                    $post->totalVotes = 0;
+                    $post->totalComments = 0;
+                } else {
+                    $post->title = $request->getParam("title", "");
+                    $post->body = $request->getParam("body", "");
+                }
+                $post->add(new \Sumidero\Database\DB($this));
+                return $response->withJson(['id' => $post->id, 'title' => $post->title, 'image' => $post->thumbnail, 'body' => $post->body ], 200);
+            });
+
         })->add(new \Sumidero\Middleware\CheckAuth($this->getContainer()));
+
+        /* post */
+
+        $this->get('/posts', function (Request $request, Response $response, array $args) {
+            $data = \Sumidero\Post::search(new \Sumidero\Database\DB($this), 1, 16, array(), "");
+            return $response->withJson(['posts' => $data->results ], 200);
+        });
 
     })->add(new \Sumidero\Middleware\APIExceptionCatcher($this->app->getContainer()));
 
     /*
     $this->app->group("/api", function() {
 
-        $this->get('/posts', function (Request $request, Response $response, array $args) {
-            $data = \Sumidero\Post::search(new \Sumidero\Database\DB($this), 1, 16, array(), "random");
-            return $response->withJson(['posts' => $data->results ], 200);
-        });
-
         $this->post('/post/scrap', function (Request $request, Response $response, array $args) {
             $data = (new \Sumidero\Scraper())->scrap($request->getParam("url", ""));
             return $response->withJson(['title' => $data["title"], 'image' => $data["image"], 'body' => $data["article"]->textContent ? trim($data["article"]->textContent): null ], 200);
         });
 
-        $this->post('/post/add', function (Request $request, Response $response, array $args) {
-            $dbh = new \Sumidero\Database\DB($this);
-            $user = \Sumidero\User::getRandom();
-            $user->add($dbh);
-            $externalUrl = $request->getParam("externalUrl", "");
-            if (! empty($externalUrl)) {
-                $subReddits = array("all", "todayilearned", "gaming", "movies", "mildlyinteresting", "science", "OldSchoolCool");
-                shuffle($subReddits);
-                $data = (new \Sumidero\Scraper())->scrap($externalUrl);
-                $post = new \Sumidero\Post(
-                    $id = (\Ramsey\Uuid\Uuid::uuid4())->toString(),
-                    $user,
-                    $externalUrl . uniqid(),
-                    parse_url($externalUrl, PHP_URL_HOST),
-                    $externalUrl,
-                    $subReddits[array_rand($subReddits)],
-                    $data["title"],
-                    $data["article"]->textContent ? substr(trim($data["article"]->textContent), 0, 384): "",
-                    $data["image"] ? $data["image"]: ""
-                );
-                $post->add($dbh);
-            }
-            return $response->withJson(['id' => $post->id, 'title' => $post->title, 'image' => $post->thumbnail, 'body' => $post->body ], 200);
-        });
     })->add(new \Sumidero\Middleware\APIExceptionCatcher($this->app->getContainer()));
     */
 ?>
