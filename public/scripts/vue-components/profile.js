2d6288b61c17ccf7a329dc72911bb3fda32da7b5
alex <766f6964+github@gmail.com>
2017-12-09 17:55:10 +0100

    new (empty) profile component

---
+++ public/scripts/vue-components/profile.js
@@ -0,0 +1,19 @@
+var profile = (function () {
+    "use strict";
+
+    var template = function () {
+        return `
+    `;
+    };
+
+    /* signIn component */
+    var module = Vue.component('sumidero-profile-component', {
+        template: template(),
+        data: function () {
+            return ({
+            });
+        }
+    });
+
+    return (module);
+})();
