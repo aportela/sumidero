9417d26cc36b78ac28951bd86a44608f07f2552f
alex <766f6964+github@gmail.com>
2018-03-25 16:54:17 +0200

    compact mode false by default

--- public/scripts/vue-components/container.js
+++ public/scripts/vue-components/container.js
@@ -24,6 +24,7 @@ var container = (function () {
         template: template(),
         data: function () {
             return ({
+                compact: false
             });
         }
     });
