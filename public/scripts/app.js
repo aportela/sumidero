579a80e35d4c9d79fe48f9f70b2c1896c1add2b3
alex <766f6964+github@gmail.com>
2019-02-11 21:00:30 +0100

    use new bus, router & api modules

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,75 +1,11 @@
-"use strict";
+import { bus } from './modules/bus.js';
+import { default as sumideroAPI } from './modules/api.js';
+import { router as router } from './modules/router.js';
 
 const imageLazyLoadObserver = lozad(); // lazy loads elements with default selector as '.lozad'
 if (imageLazyLoadObserver) {
     imageLazyLoadObserver.observe();
 }
-
-/**
- * global object for events between vuejs components
- */
-const bus = new Vue();
-
-/**
- * vue-router route definitions
- */
-const routes = [
-    { path: '/upgrade', name: 'upgrade', component: upgrade },
-    {
-        path: '/',
-        name: 'root',
-        //component: sumideroPosts,
-        component: container,
-        children: [
-            { path: '/profile', name: 'profile', component: profile },
-            { path: '/add_post', name: 'addPost', component: sumideroAddPost },
-            { path: '/update_post/:permalink', name: 'updatePost', component: sumideroUpdatePost },
-            {
-                path: '/post',
-                name: 'viewPost',
-                component: postDetails,
-                children: [
-                    {
-                        path: ':permalink',
-                        name: 'customPost',
-                        component: postDetails
-                    }
-                ]
-            },
-            {
-                path: '/s',
-                name: 'allSubs',
-                component: sumideroPosts,
-                children: [
-                    {
-                        path: ':sub',
-                        name: 'customSub',
-                        component: sumideroPosts
-                    }
-                ],
-            },
-            {
-                path: '/t',
-                name: 'allTags',
-                component: sumideroPosts,
-                children: [
-                    {
-                        path: ':tag',
-                        name: 'customTag',
-                        component: sumideroPosts
-                    }
-                ]
-            }
-        ]
-    }
-];
-
-/**
- * main vue-router component inicialization
- */
-const router = new VueRouter({
-    routes
-});
 
 /**
  * top scroll window before change router page
