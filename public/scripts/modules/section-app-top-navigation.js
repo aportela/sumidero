29f89dd65564e3b3f0b39bcf8f60d3f77eadf29a
alex <766f6964+github@gmail.com>
2019-02-15 12:59:54 +0100

    splitted navigation on separated module

---
+++ public/scripts/modules/section-app-top-navigation.js
@@ -0,0 +1,104 @@
+import { bus } from './bus.js';
+import { mixinRoutes, mixinSession  } from "../mixins.js";
+
+const template = `
+    <nav class="navbar is-fixed-top" role="navigation" aria-label="main navigation">
+        <div class="navbar-brand is-unselectable">
+            <a class="navbar-item is-uppercase has-text-weight-bold" href="https://github.com/aportela/sumidero" target="_blank">
+                <span class="icon">
+                    <i class="fab fa-github"></i>
+                </span>
+                <span>SUMIDERO</span>
+            </a>
+            <div class="navbar-item">
+                <div class="field">
+                    <div class="control has-icons-left" v-bind:class="{ 'has-icons-right, is-loading': isSearching }">
+                        <span class="icon is-small is-left">
+                            <i class="fas fa-search"></i>
+                        </span>
+                        <input ref="search" class="input is-rounded" type="text" placeholder="search..." v-bind:disabled="isSearching" v-on:keyup.enter="search();" v-on:keyup.esc="searchText = null;" v-model.trim="searchText">
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="navbar-end">
+            <div class="navbar-item">
+                <div class="buttons">
+                    <a class="button is-light" v-on:click.prevent="navigateTo('timeline')">
+                        <span class="icon">
+                            <i class="fas fa-list-alt"></i>
+                        </span>
+                        <span>timeline</span>
+                    </a>
+                </div>
+            </a>
+
+            </div>
+            <div class="navbar-item" v-if="isLogged">
+                <div class="buttons">
+                    <a class="button is-light">
+                        <span class="icon">
+                            <i class="fas fa-comment-medical"></i>
+                        </span>
+                        <span>post</span>
+                    </a>
+                    <a class="button is-light" v-on:click.prevent="navigateTo('myProfile')">
+                        <span class="icon">
+                            <i class="fas fa-user-circle"></i>
+                        </span>
+                        <span>my profile</span>
+                    </a>
+                    <a class="button is-light" v-on:click.prevent="signOut()">
+                        <span class="icon">
+                            <i class="fas fa-sign-out-alt"></i>
+                        </span>
+                        <span>sign out</span>
+                    </a>
+                </div>
+            </div>
+            <div class="navbar-item" v-else>
+                <div class="buttons">
+                    <a class="button is-light" v-on:click.prevent="navigateTo('signIn')">
+                        <span class="icon">
+                            <i class="fas fa-user-secret"></i>
+                        </span>
+                        <span>sign in</span>
+                    </a>
+                    <a class="button is-light" v-if="allowSignUp" v-on:click.prevent="navigateTo('signUp')">
+                        <span class="icon">
+                            <i class="fas fa-user-plus"></i>
+                        </span>
+                        <span>sign up</span>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </nav>
+`;
+
+export default {
+    name: 'sumidero-section-app-top-navigation',
+    template: template,
+    data: function () {
+        return ({
+            isSearching: false,
+            searchText: null
+        });
+    },
+    mixins: [
+        mixinSession, mixinRoutes
+    ],
+    methods: {
+        search: function () {
+            var self = this;
+            self.isSearching = true;
+            setTimeout(function () {
+                self.isSearching = false;
+                self.$nextTick(() => self.$refs.search.focus());
+            }, 500);
+        },
+        signOut: function () {
+            bus.$emit('signOut');
+        }
+    }
+}
