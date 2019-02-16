0f6093e85562f2594c19bf83782f88520079da93
alex <766f6964+github@gmail.com>
2019-02-16 13:43:26 +0100

    add shout / link is working

--- public/scripts/modules/section-add.js
+++ public/scripts/modules/section-add.js
@@ -1,5 +1,8 @@
 import { bus } from './bus.js';
 import { default as sumideroAPI } from './api.js';
+import { default as validator } from './validator.js';
+import { mixinRoutes, mixinSession } from '../mixins.js';
+import { uuid } from './utils.js';
 
 const template = `
     <section class="hero is-fullheight is-light is-bold">
@@ -11,7 +14,7 @@ const template = `
                         <p class="title has-text-centered" v-if="isLink">Add link</p>
                         <div class="card">
                             <div class="card-content">
-                                <form v-on:submit.prevent="onSubmit" v-if="! success">
+                                <form v-on:submit.prevent="onSubmit">
                                     <div class="field is-horizontal" v-if="isLink">
                                         <div class="field-label">
                                             <label class="label">Remote URL</label>
@@ -19,22 +22,19 @@ const template = `
                                         <div class="field-body">
                                             <div class="field is-expanded">
                                                 <div class="field has-addons">
-                                                    <div class="control is-expanded" v-bind:class="urlExists ? 'has-icons-right': ''">
-                                                        <input :disabled="loading" class="input" v-bind:class="urlExists ? 'is-danger': ''" v-model.trim="externalUrl" type="text" placeholder="https://...">
-                                                        <span class="icon is-small is-right" v-if="urlExists">
-                                                            <i class="fa fa-warning"></i>
-                                                        </span>
+                                                    <div class="control is-expanded" v-bind:class="{ 'has-icons-right' : validator.hasInvalidField('externalUrl') }">
+                                                        <input :disabled="loading" class="input" v-bind:class="{ 'is-danger': validator.hasInvalidField('externalUrl') }" v-model.trim="externalUrl" type="text" maxlength="2048" placeholder="https://...">
+                                                        <span class="icon is-small is-right" v-show="validator.hasInvalidField('externalUrl')"><i class="fa fa-warning"></i></span>
                                                     </div>
-
                                                     <div class="control">
-                                                        <a class="button is-info" :disabled="loading" v-bind:class="loading ? 'is-loading' : ''" v-on:click.prevent="scrap();">
+                                                        <a class="button is-info" :disabled="loading || ! isValidUrl" v-bind:class="loading ? 'is-loading' : ''" v-on:click.prevent="scrap();">
                                                             <span class="icon"><i class="fa fa-globe"></i></span>
                                                             <span>Scrap</span>
                                                         </a>
                                                     </div>
 
                                                 </div>
-                                                <p v-if="urlExists" class="help is-danger">Link already exists</p>
+                                                <p class="help is-danger" v-show="validator.hasInvalidField('externalUrl')">{{ validator.getInvalidFieldMessage('externalUrl') }}</p>
                                             </div>
                                         </div>
                                     </div>
@@ -45,7 +45,7 @@ const template = `
                                         <div class="field-body">
                                             <div class="field">
                                                 <div class="control">
-                                                    <input :disabled="loading" class="input loading" v-model.trim="title" type="text" placeholder="type your new post title" required>
+                                                    <input :disabled="loading" class="input loading" v-model.trim="title" type="text" placeholder="type your new post title" maxlength="128">
                                                 </div>
                                             </div>
                                         </div>
@@ -57,7 +57,7 @@ const template = `
                                         <div class="field-body">
                                             <div class="field">
                                                 <div class="control">
-                                                    <textarea :disabled="loading" class="textarea" v-model.trim="body" placeholder="type a small resume/body of your post"></textarea>
+                                                    <textarea :disabled="loading" class="textarea" v-model.trim="body" placeholder="type a small resume/body of your post" maxlength="16384"></textarea>
                                                 </div>
                                             </div>
                                         </div>
@@ -69,7 +69,19 @@ const template = `
                                         <div class="field-body">
                                             <div class="field">
                                                 <div class="control">
-                                                    <input :disabled="loading" v-model.trim="thumbnail" class="input loading" type="text" placeholder="type thumbnail url">
+                                                    <input :disabled="loading" v-model.trim="thumbnail" class="input loading" type="text" placeholder="type thumbnail url" maxlength="2048">
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="field is-horizontal" v-if="thumbnail">
+                                        <div class="field-label">
+                                            <label class="label">Thumbnail preview</label>
+                                        </div>
+                                        <div class="field-body">
+                                            <div class="columns">
+                                                <div class="column is-half is-offset-3">
+                                                    <img rel="noreferrer" v-bind:src="thumbnail" alt="Post thumbnail">
                                                 </div>
                                             </div>
                                         </div>
@@ -81,7 +93,7 @@ const template = `
                                         <div class="field-body">
                                             <div class="field">
                                                 <div class="control">
-                                                    <input :disabled="loading" class="input loading" v-model.trim="sub" type="text" placeholder="type sub name" required>
+                                                    <input :disabled="loading" class="input loading" v-model.trim="sub" type="text" placeholder="type sub name" required maxlength="32">
                                                 </div>
                                             </div>
                                         </div>
@@ -98,16 +110,15 @@ const template = `
                                             </div>
                                         </div>
                                     </div>
-                                    <div class="field is-horizontal" v-if="thumbnail">
+                                    <div class="field is-horizontal">
                                         <div class="field-label">
-                                            <label class="label">Thumbnail preview</label>
+                                            <label class="label"></label>
                                         </div>
                                         <div class="field-body">
-                                            <div class="columns">
-                                                <div class="column">
-                                                    <img rel="noreferrer" class="s-thumbnail-preview" v-bind:src="thumbnail" alt="Post thumbnail">
-                                                </div>
-                                            </div>
+                                            <button type="submit" class="button is-link is-fullwidth" v-bind:class="{ 'is-loading': loading }" v-bind:disabled="loading">
+                                                <span class="icon"><i class="fas fa-save"></i></span>
+                                                <span>Save</span>
+                                            </button>
                                         </div>
                                     </div>
                                 </form>
@@ -125,9 +136,8 @@ export default {
     template: template,
     data: function () {
         return ({
-            success: false,
+            validator: validator,
             loading: false,
-            apiError: null,
             title: null,
             externalUrl: null,
             title: null,
@@ -146,7 +156,13 @@ export default {
         },
         isValidUrl: function () {
             return (this.externalUrl && (this.externalUrl.indexOf("http://") == 0 || this.externalUrl.indexOf("https://")) == 0);
-        }, tags: function () {
+        }
+    },
+    mixins: [
+        mixinRoutes, mixinSession
+    ],
+    methods: {
+        parseTags: function () {
             if (this.tagNames) {
                 return (this.tagNames.split(",").map(function (item) {
                     return (item.trim());
@@ -155,30 +171,45 @@ export default {
                 return ([]);
             }
         },
-        urlExists: function () {
-            return (false);
-            return (this.apiError && this.apiError.status == 409 && this.apiError.body.keyAlreadyExists == 'externalUrl');
-        }
-    },
-    methods: {
         scrap: function () {
             var self = this;
+            self.validator.clear();
             self.loading = true;
-            self.apiError = null;
-            sumideroAPI.scrap(this.externalUrl, function(response) {
+            sumideroAPI.scrap(this.externalUrl, function (response) {
                 if (response.ok) {
-                    self.title = response.body.title ? response.body.title: null;
-                    self.body = response.body.body ? response.body.body: null;
-                    self.thumbnail = response.body.image? response.body.image : null;
-                    //self.tagNames = response.body.suggestedTags && response.body.suggestedTags.length > 0 ? response.body.suggestedTags.join(","): null;
-                    self.images = response.body.images;
+                    self.title = response.body.title ? response.body.title : null;
+                    self.body = response.body.body ? response.body.body : null;
+                    self.thumbnail = response.body.image ? response.body.image : null;
+                    self.tagNames = response.body.suggestedTags && response.body.suggestedTags.length > 0 ? response.body.suggestedTags.join(",") : null;
+                    self.loading = false;
                 } else {
-                    self.apiError = response;
+                    self.validator.setInvalid("externalUrl", "Remote URL scraping failed");
                 }
                 self.loading = false;
             });
         },
         onSubmit: function () {
+            var self = this;
+            self.validator.clear();
+            self.loading = true;
+            sumideroAPI.post.add(uuid(), this.externalUrl, this.title, this.body, this.sub, this.parseTags(), this.thumbnail, function (response) {
+                if (response.ok) {
+                } else {
+                    switch (response.status) {
+                        case 409:
+                            if (response.body.invalidParams.find(function (e) { return (e === "externalUrl"); })) {
+                                self.validator.setInvalid("externalUrl", "URL already exists");
+                            } else {
+                                self.showApiError(response.getApiErrorData());
+                            }
+                            break;
+                        default:
+                            self.showApiError(response.getApiErrorData());
+                            break;
+                    }
+                }
+                self.loading = false;
+            });
         }
     }
 }
