f92a30842e310dbb14017f8e1145f5a5b5c7d94d
alex <766f6964+github@gmail.com>
2019-02-19 21:11:55 +0100

    do not update parent data on event

--- public/scripts/modules/timeline-pagination.js
+++ public/scripts/modules/timeline-pagination.js
@@ -1,40 +1,44 @@
 const template = function () {
     return `
-        <nav class="pagination has-text-weight-normal is-left" role="navigation" aria-label="pagination">
-            <button type="button" class="pagination-previous" title="Click for navigate to previous page" v-show="isEnabled" v-bind:disabled="disabled || this.data.currentPage <= 1" v-on:click.prevent="navigateToPreviousPage"><span class="icon is-small"><i class="fas fa-caret-left"></i></span> <span>Previous page</span></button>
-            <button type="button" class="pagination-next" title="Click for navigate to next page" v-show="isEnabled" v-bind:disabled="disabled || this.data.currentPage >= this.data.totalPages" v-on:click.prevent="navigateToNextPage">Next page <span class="icon is-small"><i class="fas fa-caret-right"></i></span></button>
-            <ul class="pagination-list" v-show="isEnabled">
-                <!-- vuejs pagination inspired by Jeff (https://stackoverflow.com/a/35706926) -->
-                <li v-for="pageNumber in data.totalPages" v-if="pageNumber < 3 || Math.abs(pageNumber - data.currentPage) < 3 || data.totalPages - 2 < pageNumber">
-                    <a href="#" title="Click for navigate to this index page" v-bind:disabled="disabled" v-on:click.prevent="navigateToCustomPage(pageNumber)" class="pagination-link" v-bind:class="{ 'is-current': data.currentPage === pageNumber }">{{ pageNumber }}</a>
-                </li>
-            </ul>
-            <div class="field has-addons">
-                <div class="control has-icons-left">
-                    <div class="select">
-                        <select v-model.number="resultsPage" v-bind:disabled="disabled">
-                            <option value="1">1 result/page</option>
-                            <option value="2">2 results/page</option>
-                            <option value="4">4 results/page</option>
-                            <option value="8">8 results/page</option>
-                            <option value="16">16 results/page</option>
-                            <option value="32">32 results/page</option>
-                            <option value="64">64 results/page</option>
-                            <option value="128">128 results/page</option>
-                            <option value="256">256 results/page</option>
-                            <option value="256">512 results/page</option>
-                            <option value="0">All results (disable pagination)</option>
-                        </select>
+        <div class="container">
+            <hr>
+            <nav class="pagination has-text-weight-normal is-left" role="navigation" aria-label="pagination">
+                <button type="button" class="pagination-previous" title="Click for navigate to previous page" v-show="isEnabled" v-bind:disabled="disabled || this.data.currentPage <= 1" v-on:click.prevent="navigateToPreviousPage"><span class="icon is-small"><i class="fas fa-caret-left"></i></span> <span>Previous page</span></button>
+                <button type="button" class="pagination-next" title="Click for navigate to next page" v-show="isEnabled" v-bind:disabled="disabled || this.data.currentPage >= this.data.totalPages" v-on:click.prevent="navigateToNextPage">Next page <span class="icon is-small"><i class="fas fa-caret-right"></i></span></button>
+                <ul class="pagination-list" v-show="isEnabled">
+                    <!-- vuejs pagination inspired by Jeff (https://stackoverflow.com/a/35706926) -->
+                    <li v-for="pageNumber in data.totalPages" v-if="pageNumber < 3 || Math.abs(pageNumber - data.currentPage) < 3 || data.totalPages - 2 < pageNumber">
+                        <a href="#" title="Click for navigate to this index page" v-bind:disabled="disabled" v-on:click.prevent="navigateToCustomPage(pageNumber)" class="pagination-link" v-bind:class="{ 'is-current': data.currentPage === pageNumber }">{{ pageNumber }}</a>
+                    </li>
+                </ul>
+                <div class="field has-addons">
+                    <div class="control has-icons-left">
+                        <div class="select">
+                            <select v-model.number="resultsPage" v-bind:disabled="disabled">
+                                <option value="1">1 result/page</option>
+                                <option value="2">2 results/page</option>
+                                <option value="4">4 results/page</option>
+                                <option value="8">8 results/page</option>
+                                <option value="16">16 results/page</option>
+                                <option value="32">32 results/page</option>
+                                <option value="64">64 results/page</option>
+                                <option value="128">128 results/page</option>
+                                <option value="256">256 results/page</option>
+                                <option value="256">512 results/page</option>
+                                <option value="0">All results (disable pagination)</option>
+                            </select>
+                        </div>
+                        <span class="icon is-medium is-left">
+                            <i class="fas fa-list-ol"></i>
+                        </span>
                     </div>
-                    <span class="icon is-medium is-left">
-                        <i class="fas fa-list-ol"></i>
-                    </span>
+                    <div class="control">
+                        <span class="button is-static">Page {{ data.currentPage }} of {{ data.totalPages }} ({{ data.totalResults }} total result/s)</span>
+                    </div>
                 </div>
-                <div class="control">
-                    <span class="button is-static">Page {{ data.currentPage }} of {{ data.totalPages }} ({{ data.totalResults }} total result/s)</span>
-                </div>
-            </div>
-        </nav>
+            </nav>
+            <hr>
+        </div>
     `;
 };
 
@@ -68,27 +72,23 @@ export default {
     },
     watch: {
         resultsPage: function (v) {
-            this.data.resultsPage = parseInt(v);
-            this.$emit("change", { currentPage: 1, resultsPage: this.data.resultsPage });
+            this.$emit("change", { currentPage: 1, resultsPage: parseInt(v) });
         }
     },
     methods: {
         navigateToPreviousPage: function () {
             if (this.data.currentPage > 1) {
-                this.data.currentPage--;
-                this.$emit("change", { currentPage: this.data.currentPage, resultsPage: this.data.resultsPage });
+                this.$emit("change", { currentPage: this.data.currentPage- 1, resultsPage: this.data.resultsPage });
             }
         },
         navigateToNextPage: function () {
             if (this.data.currentPage < this.data.totalPages) {
-                this.data.currentPage++
-                this.$emit("change", { currentPage: this.data.currentPage, resultsPage: this.data.resultsPage });
+                this.$emit("change", { currentPage: this.data.currentPage+1, resultsPage: this.data.resultsPage });
             }
         },
         navigateToCustomPage: function (pageIdx) {
             if (pageIdx > 0 && pageIdx <= this.data.totalPages) {
-                this.data.currentPage = pageIdx;
-                this.$emit("change", { currentPage: this.data.currentPage, resultsPage: this.data.resultsPage });
+                this.$emit("change", { currentPage: pageIdx, resultsPage: this.data.resultsPage });
             }
         }
     }
