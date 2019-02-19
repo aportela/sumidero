71556b8a573151d39a1cb21a5ac4499d31e3f057
alex <766f6964+github@gmail.com>
2019-02-19 20:06:23 +0100

    added pagination support (not finished) and re-enabled lazy loading for avatars & thumbnails

--- public/scripts/modules/section-timeline.js
+++ public/scripts/modules/section-timeline.js
@@ -1,10 +1,12 @@
 import { bus } from './bus.js';
 import { default as sumideroAPI } from './api.js';
+import { default as sumideroTimelinePagination } from "./timeline-pagination.js";
 import { default as sumideroTimelinePostItem } from "./timeline-post-item.js";
 import { mixinRoutes } from './mixins.js';
 
 const template = `
     <div class="container">
+        <sumidero-timeline-pagination v-bind:data="pager" v-on:change="refreshFromPager($event.currentPage, $event.resultsPage)"></sumidero-timeline-pagination>
         <div v-if="! loading" class="sumidero-post" v-for="post in posts">
             <sumidero-timeline-post-item v-bind:post="post" v-on:onDelete="removeItemFromList($event)"></sumidero-timeline-post-item>
             <hr>
@@ -18,8 +20,16 @@ export default {
     data: function () {
         return ({
             loading: false,
+            pager: {
+                currentPage: 1,
+                previousPage: 1,
+                nextPage: 1,
+                totalPages: 0,
+                resultsPage: initialState.defaultResultsPage,
+            },
             posts: [],
-            gallery: false
+            gallery: false,
+            imageLazyLoadObserver: null
         });
     },
     props: ['sub', 'tag', 'compact'],
@@ -29,22 +39,24 @@ export default {
         }
     },
     created: function () {
-        this.loadItems();
         var self = this;
         bus.$on("search", function (text) {
             self.loadItems(text);
         });
-        bus.$on("refreshTimeline", function(text) {
+        bus.$on("refreshTimeline", function (text) {
             self.loadItems(text);
         });
+        this.imageLazyLoadObserver = lozad();
+        this.loadItems();
     },
     updated: function () {
-        //imageLazyLoadObserver.observe();
+        this.imageLazyLoadObserver.observe();
     },
     mixins: [
         mixinRoutes
     ],
     components: {
+        'sumidero-timeline-pagination': sumideroTimelinePagination,
         'sumidero-timeline-post-item': sumideroTimelinePostItem
     },
     methods: {
@@ -52,9 +64,19 @@ export default {
             var self = this;
             self.posts = [];
             self.loading = true;
-            sumideroAPI.post.search(null, this.$route.params.userId, this.$route.params.sub, this.$route.params.tag, null, initialState.session.nsfw, this.$route.params.domain, 1, 16, "creationTimestamp", "DESC", function (response) {
+            const filter = {
+                userId: this.$route.params.userId,
+                sub: this.$route.params.sub,
+                tag: this.$route.params.tag,
+                nsfw: initialState.session.nsfw,
+                domain: this.$route.params.domain,
+            }
+            sumideroAPI.post.search(this.pager.currentPage, this.pager.resultsPage, "creationTimestamp", "DESC", filter, function (response) {
                 if (response.ok) {
-                    self.posts = response.body.posts;
+                    self.posts = response.body.data.results;
+                    self.pager.currentPage = response.body.data.pagination.currentPage;
+                    self.pager.totalPages = response.body.data.pagination.totalPages;
+                    self.pager.totalResults = response.body.data.pagination.totalResults;
                     self.loading = false;
                 } else {
                     self.showApiError(response.getApiErrorData());
@@ -63,6 +85,11 @@ export default {
         },
         removeItemFromList: function (id) {
             this.posts = this.posts.filter((post) => post.id !== id);
+        },
+        refreshFromPager: function (currentPage, resultsPage) {
+            this.pager.currentPage = currentPage;
+            this.pager.resultsPage = resultsPage;
+            this.loadItems(null);
         }
     }
 }
